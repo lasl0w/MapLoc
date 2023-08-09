dfcd09d3c99e54fae7fcd10574aa74b83231aa2e
Tom Montgomery <montgomery3681@gmail.com>
2023-08-08 22:17:11 -0600

    Initial Commit

---
+++ MapLoc/MapLocApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocApp.swift
+//  MapLoc
+//
+//  Created by tom montgomery on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MapLocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
